232c76f64f5c701d5e6f189ff32603183e955ee6
Lakshmi durga Muppa <106872058+durgamuppa24@users.noreply.github.com>
2023-06-20 21:08:40 +0530

    init commit

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  api_key = 'c8e47ce6a210490a81ccdb275febaa44';
+  constructor(private http: HttpClient) { }
+
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+}
